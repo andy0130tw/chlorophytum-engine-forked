8abb59215400c896d57e87358a859b99a155a0c5
Andy Pan <andy0130tw@yahoo.com.tw>
2026-07-02 13:03:01 +0800

    Pathc hint-store-provider-file as well

--- packages/hint-store-provider-file/src/index.ts
+++ packages/hint-store-provider-file/src/index.ts
@@ -1,7 +1,21 @@
+import * as stream from "stream";
+
 import { Plugins } from "@chlorophytum/arch";
 
 import { HintStoreFsProvider } from "./provider";
 
+export interface IReadableStreamProvider {
+	createReadStream(path: string): Promise<stream.Readable>;
+}
+
+export interface IWritableStreamProvider {
+	createWriteStream(path: string): Promise<stream.Writable>;
+}
+
+export interface IStreamProvider extends IReadableStreamProvider, IWritableStreamProvider {}
+
 export const HintStoreProviderPlugin: Plugins.IHintStoreProviderPlugin = {
-	load: async () => new HintStoreFsProvider()
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	load: async (loader: Plugins.IAsyncModuleLoader, parameters: any, sp?: IStreamProvider) =>
+		await HintStoreFsProvider.create(sp)
 };
--- packages/hint-store-provider-file/src/provider.ts
+++ packages/hint-store-provider-file/src/provider.ts
@@ -1,4 +1,3 @@
-import * as fs from "fs";
 import * as stream from "stream";
 
 import {
@@ -11,23 +10,45 @@ import {
 import { MemoryHintStore } from "@chlorophytum/hint-store-memory";
 import { StreamJsonZip } from "@chlorophytum/util-json";
 
+import { IStreamProvider, IWritableStreamProvider } from ".";
+
+async function createNodeStreamProvider() {
+	const fs = await import("node:fs");
+	return {
+		async createReadStream(path: string) {
+			return fs.createReadStream(path);
+		},
+		async createWriteStream(path: string) {
+			return fs.createWriteStream(path);
+		}
+	};
+}
+
 export class HintStoreFsProvider implements IHintStoreProvider {
+	constructor(private readonly sp: IStreamProvider) {}
 	public async connectRead(path: string, pass: IHintingPass) {
-		const hs = new FsHintStore(path);
-		await new OtdHsSupport().populateHintStore(fs.createReadStream(path), pass, hs);
+		const hs = new FsHintStore(path, this.sp);
+		await new OtdHsSupport().populateHintStore(await this.sp.createReadStream(path), pass, hs);
 		return hs;
 	}
 	public async connectWrite(path: string, pass: IHintingPass) {
-		return new FsHintStore(path);
+		return new FsHintStore(path, this.sp);
+	}
+
+	static async create(sp?: IStreamProvider) {
+		return new HintStoreFsProvider(sp ?? (await createNodeStreamProvider()));
 	}
 }
 
 class FsHintStore extends MemoryHintStore {
-	constructor(private saveTo: string) {
+	constructor(
+		private saveTo: string,
+		private readonly wsp: IWritableStreamProvider
+	) {
 		super();
 	}
 	public async commitChanges() {
-		const output = fs.createWriteStream(this.saveTo);
+		const output = await this.wsp.createWriteStream(this.saveTo);
 		await new OtdHsSupport().saveHintStore(this, output);
 	}
 }
